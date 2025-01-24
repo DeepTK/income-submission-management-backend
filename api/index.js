f2176baf8ff2dc149c8e387b0e8126dafc646988
Deepak <deep101kamtekar@gmail.com>
2025-01-24 17:53:11 +0530

    minor change

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-const app = require('../app');
-const serverless = require('serverless-http');
+const app = require("../app");
+const serverless = require("serverless-http");
+
+const handler = serverless(app, {
+  binary: ["*/*"],
+  timeout: 30,
+});
 
 module.exports = serverless(app);
