5e6e6367941033834dac53c65d18113ce4e2b442
Deepak Kamtekar <97279264+DeepTK@users.noreply.github.com>
2025-01-24 17:58:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const app = require("../app");
-const serverless = require("serverless-http");
+const serverless = require('serverless-http');
+const app = require('../app');
 
 const handler = serverless(app, {
-  binary: ["*/*"],
-  timeout: 30,
+  binary: ['*/*'],
+  timeout: 30
 });
 
-module.exports = serverless(app);
+module.exports = handler;
